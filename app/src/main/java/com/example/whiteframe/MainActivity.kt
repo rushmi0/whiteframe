5486c1d7cb59df61fd35f17a627e3d9a2c53d616
rushmi0 <gcalku@gmail.com>
2025-05-03 21:41:39 +0700

    Arrangements and Alignments

--- app/src/main/java/com/example/whiteframe/MainActivity.kt
+++ app/src/main/java/com/example/whiteframe/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.whiteframe.ui.theme.WhiteFrameTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +34,98 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WhiteFrameTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyAlignments()
             }
         }
     }
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = Modifier.fillMaxSize()
-    )
+private fun MyAlignments() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello",
+            color = Color.White,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.Red)
+                .padding(start = 20.dp, end = 20.dp, top = 5.dp, bottom = 5.dp)
+        )
+        Spacer(modifier = Modifier.size(20.dp))
+
+        Text(
+            text = "Kotlin",
+            color = Color.White,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.Red)
+                .padding(start = 20.dp, end = 20.dp, top = 5.dp, bottom = 5.dp)
+        )
+        Spacer(modifier = Modifier.size(20.dp))
+
+        Text(
+            text = "Android",
+            color = Color.White,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.Red)
+                .padding(start = 20.dp, end = 20.dp, top = 5.dp, bottom = 5.dp)
+        )
+        Spacer(modifier = Modifier.size(20.dp))
+
+    }
+}
+
+
+@Composable
+fun MyLayouts(name: String) {
+    Column {
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+                .height(200.dp)
+                .width(300.dp)
+        ) {
+            Row {
+
+                Box(
+                    modifier = Modifier
+                        .size(60.dp)
+                        .background(Color.Blue)
+                )
+                Column(modifier = Modifier.background(Color.Green)) {
+                    Text(text = "Hello")
+                    Text(text = "$name!")
+                }
+            }
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     WhiteFrameTheme {
-        Greeting("Android")
+        //Greeting("Android")
+        //MyLayouts("Android")
+        MyAlignments()
+
     }
 }
